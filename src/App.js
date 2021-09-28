b73600a285c6c6964e4ecdd6757c1b8c7830fffd
AnjaliRaiDaffodilsw <78538422+AnjaliRaiDaffodilsw@users.noreply.github.com>
2021-09-28 08:51:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
-import routes from './routing/routes';
 import Home from './Components/Dashboard/Home';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Switch>
-          <Route path={routes.DEFAULT} component={Home} />
-        </Switch>
-      </BrowserRouter>
+      <Home />
     </div>
   );
 }
